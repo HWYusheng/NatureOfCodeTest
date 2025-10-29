379379209a1bd4d8dcda37c4ab6d6e65e2d1fe84
Thắng Hoàng Việt <thanghv2007@gmail.com>
2025-10-29 21:43:36 +0000

    Add project files.

---
+++ NatureOfCodeTest/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace NatureOfCodeTest
+{
+    internal class Mover
+    {
+        private Vector2 position;
+        private Vector2 velocity;
+        private Vector2 acceleration;
+        public Mover(float x, float y)
+        {
+            this.position = new Vector2(x, y);
+        }
+        public void ApplyForce(float fx, float fy)
+        {
+
+        }
+        public void Update()
+        {
+        }
+    }
+}
