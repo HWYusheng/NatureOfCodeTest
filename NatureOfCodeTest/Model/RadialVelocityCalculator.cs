6c4ab1480577d31d3e8243cb3022d69aeb0548f6
235274 <235274@live.brock.ac.uk>
2026-01-15 09:39:23 +0000

    added

---
+++ NatureOfCodeTest/Model/RadialVelocityCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureOfCodeTest.Model
+{
+    public class RadialVelocityCalculator
+    {
+        public Vector2 LineOfSight { get; set; } = new Vector2(0, 1); // Mặc định nhìn theo trục Y
+        public double Dot(Vector2 other, Vector2 prime) { 
+           return prime.X * other.X + prime.Y * other.Y; 
+        }
+        public double Compute(Vector2 starVelocity)
+        {
+            return Dot(LineOfSight, starVelocity);
+        }
+    }
+}
