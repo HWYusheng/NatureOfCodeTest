9432832800e1506efd461b6293299c03de50f06b
HWYusheng <118163312+HWYusheng@users.noreply.github.com>
2026-02-10 12:11:03 +0000

    api call

--- NatureOfCodeTest/API.cs
+++ NatureOfCodeTest/API.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NatureOfCodeTest.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,113 +11,37 @@ using System.Threading.Tasks;
 
 namespace NatureOfCodeTest
 {
-    public class Product
+    public static class Consts
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string Category { get; set; }
+        public static string SPOONACULAR_API_KEY = "MY_KEY";
     }
+    public class SpoonacularService
+    {
+        public async Task<IEnumerable<Star>> GetStars(String query)
+        {
+            List<Star> starList = new List<Star>();
 
-    //class Program
-    //{
-    //    static HttpClient client = new HttpClient();
+            var url = $"https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?";
+            var parameters = $"?query={query}&apiKey={Consts.SPOONACULAR_API_KEY}&number=5";
 
-    //    static void ShowProduct(Product product)
-    //    {
-    //        Console.WriteLine($"Name: {product.Name}\tPrice: " +
-    //            $"{product.Price}\tCategory: {product.Category}");
-    //    }
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-    //    static async Task<Uri> CreateProductAsync(Product product)
-    //    {
-    //        HttpResponseMessage response = await client.PostAsJsonAsync(
-    //            "api/products", product);
-    //        response.EnsureSuccessStatusCode();
+            HttpResponseMessage response = await client.GetAsync(parameters).ConfigureAwait(false);
 
-    //        // return URI of the created resource.
-    //        return response.Headers.Location;
-    //    }
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var stars = JsonConvert.DeserializeObject<List<Star>>(jsonString);
 
-    //    static async Task<Product> GetProductAsync(string path)
-    //    {
-    //        Product product = null;
-    //        HttpResponseMessage response = await client.GetAsync(path);
-    //        if (response.IsSuccessStatusCode)
-    //        {
-    //            product = await response.Content.ReadAsAsync<Product>();
-    //        }
-    //        return product;
-    //    }
+                if (stars != null)
+                {
+                    starList.AddRange(starList);
+                }
+            }
 
-    //    static async Task<Product> UpdateProductAsync(Product product)
-    //    {
-    //        HttpResponseMessage response = await client.PutAsJsonAsync(
-    //            $"api/products/{product.Id}", product);
-    //        response.EnsureSuccessStatusCode();
-
-    //        // Deserialize the updated product from the response body.
-    //        product = await response.Content.ReadAsAsync<Product>();
-    //        return product;
-    //    }
-
-    //    static async Task<HttpStatusCode> DeleteProductAsync(string id)
-    //    {
-    //        HttpResponseMessage response = await client.DeleteAsync(
-    //            $"api/products/{id}");
-    //        return response.StatusCode;
-    //    }
-
-    //    static void Main()
-    //    {
-    //        RunAsync().GetAwaiter().GetResult();
-    //    }
-
-    //    static async Task RunAsync()
-    //    {
-    //        // Update port # in the following line.
-    //        client.BaseAddress = new Uri("http://localhost:64195/");
-    //        client.DefaultRequestHeaders.Accept.Clear();
-    //        client.DefaultRequestHeaders.Accept.Add(
-    //            new MediaTypeWithQualityHeaderValue("application/json"));
-
-    //        try
-    //        {
-    //            // Create a new product
-    //            Product product = new Product
-    //            {
-    //                Name = "Gizmo",
-    //                Price = 100,
-    //                Category = "Widgets"
-    //            };
-
-    //            var url = await CreateProductAsync(product);
-    //            Console.WriteLine($"Created at {url}");
-
-    //            // Get the product
-    //            product = await GetProductAsync(url.PathAndQuery);
-    //            ShowProduct(product);
-
-    //            // Update the product
-    //            Console.WriteLine("Updating price...");
-    //            product.Price = 80;
-    //            await UpdateProductAsync(product);
-
-    //            // Get the updated product
-    //            product = await GetProductAsync(url.PathAndQuery);
-    //            ShowProduct(product);
-
-    //            // Delete the product
-    //            var statusCode = await DeleteProductAsync(product.Id);
-    //            Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
-
-    //        }
-    //        catch (Exception e)
-    //        {
-    //            Console.WriteLine(e.Message);
-    //        }
-
-    //        Console.ReadLine();
-    //    }
-    //}
+            return starList;
+        }
+    }
 }
--- NatureOfCodeTest/Model/CelestialBody.cs
+++ NatureOfCodeTest/Model/CelestialBody.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -8,14 +9,15 @@ using static NatureOfCodeTest.Model.CelestialBody;
 
 namespace NatureOfCodeTest.Model
 {
-
-        public abstract class CelestialBody
+    [Serializable]
+    public abstract class CelestialBody
         {
             public const double G = 6.67430e-11;
             public const double SpeedOfLight = 299792458;
             public const double SolarMass = 1.989e30;
             public const double AU = 1.496e11;
-            public string Name { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
             public double Mass { get; set; }
             public Vector2 Position { get; set; }
             public Vector2 Velocity { get; set; }
