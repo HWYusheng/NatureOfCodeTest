7dc5437ed6e5bee97bc7ebdedaa1ea8a4a0c5dc7
HWYusheng <118163312+HWYusheng@users.noreply.github.com>
2026-03-05 10:29:09 +0000

    dsafdsfadsf

--- NatureOfCodeTest/Model/OrbitCalculator.cs
+++ NatureOfCodeTest/Model/OrbitCalculator.cs
@@ -27,20 +27,20 @@ namespace NatureOfCodeTest.Model
             // 2. Mean Anomaly (M) at 'time'
             // M = M0 + n * (t - t0)
             double M = orbit.MeanAnomalyAtEpoch + n * (time - orbit.EpochTime);
-            M = M % (2 * Math.PI); // Chuẩn hóa về [0, 2pi]
+            M = M % (2 * Math.PI); 
 
             // 3. Solve Kepler eq to find Eccentric Anomaly (E)
             // M = E - e * sin(E) -> Use KeplerSolver (Newton-Raphson)
             double E = _solver.SolveEccentricAnomaly(M, orbit.Eccentricity);
 
-            // 4. Calc coord on Orbital Plane
+            // 4. Calc position coord on Orbital Plane
             double cosE = Math.Cos(E);
             double sinE = Math.Sin(E);
             double x_orbital = orbit.SemiMajorAxis * (cosE - orbit.Eccentricity);
             double y_orbital = orbit.SemiMajorAxis * Math.Sqrt(1 - Math.Pow(orbit.Eccentricity, 2)) * sinE;
 
-            // 5. Rotate coord according to Argument of Periapsis (ω) and Inclination (i)
-            // Đối với 2D đơn giản, chúng ta chủ yếu xoay theo ω (góc lệch của cận điểm quỹ đạo)
+            // 5. Rotate position coord according to Argument of Periapsis (ω) and Inclination (i)
+            // This is only 2D, so only need to rotate with respect to w
             double omega = orbit.ArgumentOfPeriapsis;
             double cosW = Math.Cos(omega);
             double sinW = Math.Sin(omega);
@@ -48,16 +48,16 @@ namespace NatureOfCodeTest.Model
             double x_final = x_orbital * cosW - y_orbital * sinW;
             double y_final = x_orbital * sinW + y_orbital * cosW;
 
-            // 6. Update relative coord to the Star
+            // 6. Update relative position coord to the Star
             planet.Position = new Vector2((float)(star.Position.X + x_final), (float)(star.Position.Y + y_final));
 
-            // 7. Tính vận tốc tức thời (Velocity) - Đạo hàm của vị trí
+            // 7. Calc vận tốc tức thời (Velocity) - dy/dx of position
             // v = sqrt(G * M_total / a) * [-sinE, sqrt(1-e^2)*cosE] / (1 - e*cosE)
             double v_factor = Math.Sqrt(PhysicalConstants.G * totalMass / orbit.SemiMajorAxis) / (1 - orbit.Eccentricity * cosE);
             double vx_orbital = -v_factor * sinE;
             double vy_orbital = v_factor * Math.Sqrt(1 - Math.Pow(orbit.Eccentricity, 2)) * cosE;
 
-            // Xoay vận tốc tương tự như vị trí
+            // Rotate speed same way as position
             planet.Velocity = new Vector2(
                 (float)(vx_orbital * cosW - vy_orbital * sinW),
                 (float)(vx_orbital * sinW + vy_orbital * cosW)
@@ -66,7 +66,7 @@ namespace NatureOfCodeTest.Model
 
         public Vector2 ComputeStarReflexMotion(Planet p, Star s)
         {
-            // Tính toán sự rung lắc của sao chủ do lực hấp dẫn từ hành tinh
+            // Calc the wobble of the Star caused by the planet pull
             double ratio = p.Mass / s.Mass;
             return Vector2.Multiply((float)(-ratio), p.Velocity);
         }
