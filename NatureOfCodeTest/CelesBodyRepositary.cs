8b3a22cbbd1cf2de8b7881f6b1ea8ea8ab76424e
HWYusheng <118163312+HWYusheng@users.noreply.github.com>
2026-03-20 15:55:22 +0000

    much work to the Api

--- NatureOfCodeTest/CelesBodyRepositary.cs
+++ NatureOfCodeTest/CelesBodyRepositary.cs
@@ -13,32 +13,32 @@ namespace NatureOfCodeTest
     internal class CelesBodyRepositary
     {
         string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source = " + Environment.CurrentDirectory + @"\StellarWobble.accdb";
-        public void AddPlanet(PlanetFJson planet)
+        public void AddPlanet(Planet planet)
         {
             string sql = "INSERT INTO tblPlanet (HostName, pl_Name, pl_Masse, pl_SemiMajorAxis, pl_Eccentricity, pl_Inclination, pl_ArgumentOfPeriapsis) VALUES (?, ?, ?, ?, ?, ?, ?)";
             using (OleDbConnection conn = new OleDbConnection(connectionString))
             using (OleDbCommand cmd = new OleDbCommand(sql, conn))
             {
                 cmd.Parameters.AddWithValue("@HostName", planet.HostName);
-                cmd.Parameters.AddWithValue("@pl_Name", planet.pl_Name);
-                cmd.Parameters.AddWithValue("@pl_Masse", planet.pl_Masse);
-                cmd.Parameters.AddWithValue("@pl_SemiMajorAxis", planet.pl_orbsmax);
-                cmd.Parameters.AddWithValue("@pl_Eccentricity", planet.pl_orbeccen);
-                cmd.Parameters.AddWithValue("@pl_Inclination", planet.pl_orbincl);
-                cmd.Parameters.AddWithValue("@pl_ArgumentOfPeriapsis", planet.pl_orblper);
+                cmd.Parameters.AddWithValue("@pl_Name", planet.Name);
+                cmd.Parameters.AddWithValue("@pl_Masse", planet.Mass);
+                cmd.Parameters.AddWithValue("@pl_SemiMajorAxis", planet.Orbit.SemiMajorAxis);
+                cmd.Parameters.AddWithValue("@pl_Eccentricity", planet.Orbit.Eccentricity);
+                cmd.Parameters.AddWithValue("@pl_Inclination", planet.Orbit.Inclination);
+                cmd.Parameters.AddWithValue("@pl_ArgumentOfPeriapsis", planet.Orbit.ArgumentOfPeriapsis);
                 conn.Open();
                 cmd.ExecuteNonQuery();
             }
         }
-        public void AddStar(StarFJson star)
+        public void AddStar(Star star)
         {
             string sql = "INSERT INTO tblStar (HostName, st_Mass, st_Luminosity) VALUES (?, ?, ?)";
             using (OleDbConnection conn = new OleDbConnection(connectionString))
             using (OleDbCommand cmd = new OleDbCommand(sql, conn))
             {
-                cmd.Parameters.AddWithValue("@HostName", star.HostName);
-                cmd.Parameters.AddWithValue("@st_Mass", star.st_mass);
-                cmd.Parameters.AddWithValue("@st_Luminosity", star.st_lum);
+                cmd.Parameters.AddWithValue("@HostName", star.Name);
+                cmd.Parameters.AddWithValue("@st_Mass", star.Mass);
+                cmd.Parameters.AddWithValue("@st_Luminosity", star.Luminosity);
                 conn.Open();
                 cmd.ExecuteNonQuery();
             }
@@ -48,7 +48,7 @@ namespace NatureOfCodeTest
         public List<CelestialBody> GetSystem()
         {
             List<CelestialBody> CelesBodies = new List<CelestialBody>();
-            string sqlFStar = "SELECT * FROM tblStar";
+            string sqlFStar = "SELECT * FROM tblStar, tblPlanet WHERE ";
             using (OleDbConnection conn = new OleDbConnection(connectionString))
             using (OleDbCommand cmd = new OleDbCommand(sqlFStar, conn))
             {
@@ -84,33 +84,33 @@ namespace NatureOfCodeTest
         }
         //pasted json string to json2csharp
         // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-        [Serializable]
-        public abstract class CelestialBodyFJson
-        {
-            [JsonProperty("name")]
-            public string Name { get; set; }
-            public double Mass { get; set; }
+        //[Serializable]
+        //public abstract class CelestialBodyFJson
+        //{
+        //    [JsonProperty("name")]
+        //    public string Name { get; set; }
+        //    public double Mass { get; set; }
 
-        }
-        public class PlanetFJson : CelestialBodyFJson
-        {
-            // 7 in total
-            public string HostName { get; set; }
-            public string pl_Name { get; set; }
-            public double pl_Masse { get; set; } // planet mass, how many times of earth's mass
-            public double pl_orbsmax { get; set; } // a - planet SemiMajorAxis
-            public double pl_orbeccen { get; set; }  // e - planet Eccentricity
-            public double pl_orbincl { get; set; }   // i (degrees) - planet  Inclination
-            public double pl_orblper { get; set; } // ω - planet ArgumentOfPeriapsis 
-            //  MeanAnomalyAtEpoch and EpochTime are not needed, it only used to determine the initial state, which could be anywhere when being observed in real life.
-            //  We just want to set up a certain place for simplicity.
-        }
-        public class StarFJson : CelestialBodyFJson
-        {
-            // 3 in total
-            public string HostName { get; set; } // basiclly star name
-            public double st_mass { get; set; } // star mass
-            public double st_lum { get; set; } // star luminosity
-        }
+        //}
+        //public class PlanetFJson : CelestialBodyFJson
+        //{
+        //    // 7 in total
+        //    public string HostName { get; set; }
+        //    public string pl_Name { get; set; }
+        //    public double pl_Masse { get; set; } // planet mass, how many times of earth's mass
+        //    public double pl_orbsmax { get; set; } // a - planet SemiMajorAxis
+        //    public double pl_orbeccen { get; set; }  // e - planet Eccentricity
+        //    public double pl_orbincl { get; set; }   // i (degrees) - planet  Inclination
+        //    public double pl_orblper { get; set; } // ω - planet ArgumentOfPeriapsis 
+        //    //  MeanAnomalyAtEpoch and EpochTime are not needed, it only used to determine the initial state, which could be anywhere when being observed in real life.
+        //    //  We just want to set up a certain place for simplicity.
+        //}
+        //public class StarFJson : CelestialBodyFJson
+        //{
+        //    // 3 in total
+        //    public string HostName { get; set; } // basiclly star name
+        //    public double st_mass { get; set; } // star mass
+        //    public double st_lum { get; set; } // star luminosity
+        //}
     }
 }
