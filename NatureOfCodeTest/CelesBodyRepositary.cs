b0cd44cc721add52f305e2bf698171010450a13e
HWYusheng <118163312+HWYusheng@users.noreply.github.com>
2026-03-25 13:58:42 +0000

    finished fetching data

--- NatureOfCodeTest/CelesBodyRepositary.cs
+++ NatureOfCodeTest/CelesBodyRepositary.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Numerics;
@@ -14,9 +15,14 @@ namespace NatureOfCodeTest
     internal class CelesBodyRepositary
     {
         string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source = " + Environment.CurrentDirectory + @"\StellerWobble.accdb";
+        
         public void AddPlanet(Planet planet)
         {
             int StarID = GetStarID(planet.HostName);
+            if (StarID == -1)
+            {
+                return;
+            }
             string sql = "INSERT INTO tblPlanet (StarID, pl_Name, pl_Masse, pl_orbsmax, pl_orbeccen, pl_orbincl, pl_orblper) VALUES (?, ?, ?, ?, ?, ?, ?)";
             using (OleDbConnection conn = new OleDbConnection(connectionString))
             using (OleDbCommand cmd = new OleDbCommand(sql, conn))
@@ -54,8 +60,8 @@ namespace NatureOfCodeTest
         }
         public int GetStarID(string HostName) 
         {
-            int _starID = 0;
-            string sql = "SELECT StarID FROM tblStudent WHERE HostName = ?"; // ? is a placeholder for parameters in OleDb
+            int _starID = -1;
+            string sql = "SELECT StarID FROM tblStar WHERE HostName = ?"; // ? is a placeholder for parameters in OleDb
             using (OleDbConnection conn = new OleDbConnection(connectionString))
             using (OleDbCommand cmd = new OleDbCommand(sql, conn))
             {
@@ -72,6 +78,22 @@ namespace NatureOfCodeTest
             }
 
             return _starID;
+        }
+        public void DeleteAllSQL()
+        {
+            List<string> tblList = new List<string> { "tblStar", "tblPlanet", "Simulations" };
+            foreach (var item in tblList)
+            {
+                string deleteAllSql = "DELETE FROM " + item;
+                using (OleDbConnection conn = new OleDbConnection(connectionString))
+                using (OleDbCommand cmd = new OleDbCommand(deleteAllSql, conn))
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+
         }
         // This one will try to get a whole star system. Meaning: host star + planet(s)
         // Probably a list with different name for same parameters of the 2, or try to use the celesbody. Or do a method that can store 2 different types at the same time.
