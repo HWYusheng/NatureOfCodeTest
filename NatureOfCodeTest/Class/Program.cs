093f09e42100d800a4ee16d7457ed84f5ecc2022
Thắng Hoàng Việt <thanghv2007@gmail.com>
2026-04-04 17:16:40 +0100

    ok

--- NatureOfCodeTest/Class/Program.cs
+++ NatureOfCodeTest/Class/Program.cs
@@ -1,28 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Numerics;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
 
-namespace NatureOfCodeTest
-{
-    internal static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+//namespace NatureOfCodeTest
+//{
+//    internal static class Program
+//    {
+//        /// <summary>
+//        /// The main entry point for the application.
+//        /// </summary>
+//        [STAThread]
+//        static void Main()
+//        {
+//            Application.EnableVisualStyles();
+//            Application.SetCompatibleTextRenderingDefault(false);
             
-            // Initialize DB schema and clear old guest runs
-            DatabaseSetup.InitializeDb();
+//            // Initialize DB schema and clear old guest runs
+//            DatabaseSetup.InitializeDb();
             
-            Application.Run(new Form1());
-        }
-    }
+//            Application.Run(new Form1());
+//        }
+//    }
 
-}
+//}
